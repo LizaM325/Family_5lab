5bb7a84d7151fe9c3b66ea814adfde6256323226
LizaM325 <liza.mikheeva325@yahoo.com>
2025-04-27 18:53:00 +0500

    template method

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include <vector>
 #include <memory>
 
-// Enum for drinking rules
-enum class The_drinking_rule
-{
-    in_sips,
-    use_straw,
-    use_spoon
-};
-
 class DrinkingStrategy
 {
 public:
@@ -45,7 +37,6 @@ public:
     }
 };
 
-// Tea class using strategy
 class Tea
 {
 protected:
@@ -80,12 +71,21 @@ public:
     }
 };
 
-// Concrete tea classes
 class BlackTea : public Tea
 {
 public:
     BlackTea(std::unique_ptr<DrinkingStrategy> strategy)
         : Tea("Black tea", std::move(strategy)) {}
+
+    void BrewTea() const override
+    {
+        std::cout << "Brewing black tea..." << std::endl;
+    }
+
+    void PourTea() const override
+    {
+        std::cout << "Pouring black tea..." << std::endl;
+    }
 };
 
 class GreenTea : public Tea
@@ -93,6 +93,15 @@ class GreenTea : public Tea
 public:
     GreenTea(std::unique_ptr<DrinkingStrategy> strategy)
         : Tea("Green tea", std::move(strategy)) {}
+
+    void BrewTea() const override
+    {
+        std::cout << "Brewing green tea..." << std::endl;
+    }
+
+    void PourTea() const override {
+        std::cout << "Pouring green tea..." << std::endl;
+    }
 };
 
 class HibiscusTea : public Tea
@@ -100,12 +109,21 @@ class HibiscusTea : public Tea
 public:
     HibiscusTea(std::unique_ptr<DrinkingStrategy> strategy)
         : Tea("Hibiscus tea", std::move(strategy)) {}
+
+    void BrewTea() const override
+    {
+        std::cout << "Brewing hibiscus tea..." << std::endl;
+    }
+
+    void PourTea() const override
+    {
+        std::cout << "Pouring hibiscus tea..." << std::endl;
+    }
 };
 
 int main()
 {
     std::vector<std::unique_ptr<Tea>> teas;
-
     teas.push_back(std::make_unique<BlackTea>(std::make_unique<InSipsDrinkingStrategy>()));
     teas.push_back(std::make_unique<GreenTea>(std::make_unique<UseStrawDrinkingStrategy>()));
     teas.push_back(std::make_unique<HibiscusTea>(std::make_unique<UseSpoonDrinkingStrategy>()));
